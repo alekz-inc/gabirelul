05bdb94d5c2d4ae28a6963c7c9f1ba773edd11b0
alekz-inc <alex_chera16@yahoo.com>
2022-12-13 14:50:56 +0200

    hh
    hh

--- java.js
+++ java.js
@@ -1,18 +1,14 @@
-/*==================== SHOW NAVBAR ====================*/
 const showMenu = (headerToggle, navbarId) => {
     const toggleBtn = document.getElementById(headerToggle)
     const nav = document.getElementById(navbarId)
 
-    // Validate that variables exist
+
     if (headerToggle && navbarId) {
         toggleBtn.addEventListener('click', () => {
-            // We add the show-menu class to the div tag with the nav__menu class
             nav.classList.toggle('show-menu')
             // change icon
             toggleBtn.classList.toggle("active")
         })
     }
-
-
 }
 showMenu('header-toggle', 'navbar')
